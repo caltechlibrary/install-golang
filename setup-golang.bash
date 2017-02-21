f6025d870e8ace9ae059f815318354929f1602e7
R. S. Doiel <rsdoiel@gmail.com>
2017-02-21 12:50:40 -0800

    Implemented workaround for Mac OS X Sierra broken compiler problem

--- setup-golang.bash
+++ setup-golang.bash
@@ -1,5 +1,21 @@
 #!/bin/bash
-GO_TARGET_VERSION="go1.7.5"
+
+function MacOSX_Ugly_Build_Problem() {
+    if [ ! -f /usr/local/go/bin/go ]; then
+        echo "If you are running Mac OS X Sierra compiling will be VERY problematic"
+        echo "You need to bootstrap from a compiled Binary release like go1.7.5."
+        echo "See https://golang.org/doc/install?download=go1.7.5.darwin-amd64.pkg"
+        echo ""
+        read -p "Do you want to continue? Y/N " Y_OR_N
+        if [ "$Y_OR_N" != "Y" ] && [ "$Y_OR_N" != "y" ]; then
+            exit 0
+        fi
+    fi
+    export GOROOT_BOOTSTRAP=/usr/local/go
+}
+
+START=$(pwd)
+GO_TARGET_VERSION="go1.8"
 CC=$(which cc)
 if [ "$CC" = "" ]; then
     export CGO_ENABLED=0
@@ -16,30 +32,42 @@ fi
 if [ "$GO_VERSION" = "" ]; then
     read -p "Clone, compile and (re)install Go now? Y/N " INSTALL_GO
     if [ "$INSTALL_GO" = "Y" ] || [ "$INSTALL_GO" = "y" ]; then
-        # Setup where to find go1.4
-        if [ ! -d $HOME/go1.4 ]; then
-            echo "Cloning go v1.4"
-            git clone https://github.com/golang/go go1.4
-        fi
+        if [ "${HOME:0:6}" = "/Users" ] && [ "${TERM_PROGRAM:0:5}" = "Apple" ]; then
+            # Handle Mac Ugly bootstrap problem
+            MacOSX_Ugly_Build_Problem
+        else
+            # Setup where to find go1.4
+            if [ ! -d "$HOME/go1.4" ]; then
+                echo "Cloning go v1.4"
+                git clone https://github.com/golang/go $HOME/go1.4
+            fi
 
-        # Compile go1.4 if necessary
-        if [ ! -f $HOME/go1.4/bin/go ]; then
-            echo "Checking out and compiling go 1.4.3"
-            cd $HOME/go1.4
-            git checkout go1.4.3
-            cd src
-            ./all.bash
-            cd
+            # Compile go1.4 if necessary
+            if [ ! -f "$HOME/go1.4/bin/go" ]; then
+                echo "Checking out and compiling go 1.4.3"
+                cd $HOME/go1.4
+                git checkout master
+                git fetch origin
+                git pull origin master
+                git checkout go1.4.3
+                cd src
+                ./all.bash
+                cd $HOME
+            else
+                echo "Using $($HOME/go1.4/bin/go version) to bootstrap"
+            fi
         fi
 
         # Setup where to find $GO_TARGET_VERSION
         if [ ! -d $HOME/go ]; then
             echo "Cloning go to $HOME/go"
-            git clone https://github.com/golang/go
+            git clone https://github.com/golang/go $HOME/go
         fi
 
         cd $HOME/go
+        git checkout master
         git fetch origin
+        git pull origin master
         GO_VERSION=$(git branch | grep "$GO_TARGET_VERSION")
         if [ "$GO_VERSION" = "" ]; then
             git checkout $GO_TARGET_VERSION
@@ -49,25 +77,29 @@ if [ "$GO_VERSION" = "" ]; then
         fi
         # Compile $GO_TARGET_VERSION
         if [ "$GO_VERSION" = "" ]; then
-        echo "Checking out and compiling $GO_TARGET_VERSION"
-        export CGO_ENABLED=0
-        git checkout $GO_TARGET_VERSION
-        cd src
-        ./all.bash
+            echo "Checking out and compiling $GO_TARGET_VERSION"
+            export CGO_ENABLED=0
+            git checkout $GO_TARGET_VERSION
+            cd src
+            ./all.bash
         fi
-        cd
+        cd $HOME
         export GOPATH=$HOME
     fi
 fi
+echo "Checking go version installed"
 T=$(go version 2> /dev/null)
 if [ "$T" != "" ]; then
     echo "$T"
+else
+    echo "Failed to find Go"
+    exit 1
 fi
 
-GOPHERJS=$(which gopherjs)
-if [ "$GOPHERJS" = "" ]; then
-    read -p "Install GopherJS? Y/N " Y_OR_N
-    if [ "$Y_OR_N" = "Y" ] || [ "$Y_OR_N" = "y" ]; then
-        go get -u github.com/gopherjs/gopherjs/...
-    fi
-fi
+#GOPHERJS=$(which gopherjs)
+#if [ "$GOPHERJS" = "" ]; then
+#    read -p "Install GopherJS? Y/N " Y_OR_N
+#    if [ "$Y_OR_N" = "Y" ] || [ "$Y_OR_N" = "y" ]; then
+#        go get -u github.com/gopherjs/gopherjs/...
+#    fi
+#fi
